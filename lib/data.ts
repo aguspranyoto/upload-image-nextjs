901dd32c7373e3a4a39855459bb9aa93abddd749
aguspranyoto <agusprnyt@gmail.com>
2024-05-12 09:35:48 +0700

    first commit

---
+++ lib/data.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/prisma";
+
+export const getImages = async () => {
+  try {
+    const result = await prisma.upload.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return result;
+  } catch (error) {
+    throw new Error("Failed to get images");
+  }
+};
+
+export const getImagesById = async (id: string) => {
+  try {
+    const result = await prisma.upload.findUnique({
+      where: {
+        id,
+      },
+    });
+    return result;
+  } catch (error) {
+    throw new Error("Failed to get images");
+  }
+};
